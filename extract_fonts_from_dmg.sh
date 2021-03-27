5e471dcd257d078a2f6358a4723fa64bfbd04be7
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2021-03-27 11:39:03 -0700

    extract from dmg

---
+++ extract_fonts_from_dmg.sh
@@ -0,0 +1,47 @@
+dmg_file="$1"
+
+if [ "$dmg_file" == "" ]
+then
+  echo "Usage: bash $0 <.dmg_file_to_extract>"
+  exit 1
+fi
+
+fonts_dir="fonts"
+
+function extract_fonts_from_dmg {
+  # 1. create target directory for fonts
+  local dmg_file="$1"
+  local basename="$(basename ""$dmg_file"")"
+  local basename="${basename%.*}"
+  local target_dir="$fonts_dir/$basename"
+  mkdir -pv "$target_dir"
+
+  # 2. create temporary directory to extract files
+  local tempdir=$(mktemp -d)
+
+  # 3. convert .dmg to .img, and put int in temporary directory
+  local img_file="$tempdir/$basename.img"
+  dmg2img -i "$dmg_file" -o "$img_file"
+
+  # 4. extract .img to temporary directory
+  7z x "$img_file" -O"$tempdir"
+
+  # 5. change to temporary directory
+  # extract the .pkg file there
+  pushd "$tempdir"
+  find . -name *.pkg -exec 7z x {} ";"
+
+  # 6. extract Payload~
+  local payload="Payload~"
+  find . -name "$payload" -exec 7z x {} ";"
+
+  # 7. go back to current directory
+  # move all .otf and .ttf files to target directory
+  popd
+  find "$tempdir" -name "*.[ot]tf" -exec mv {} "$target_dir" ";"
+
+  # 8. clean up temporary directory
+  rm -rf "$tempdir"
+}
+
+extract_fonts_from_dmg "$dmg_file"
