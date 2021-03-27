90a14eea19af3829676e5b0c38aab29cfe18497f
mobiledesres <45989029+mobiledesres@users.noreply.github.com>
2021-03-27 12:14:00 -0700

    extract fonts from dmg; updated Makefile

--- Makefile
+++ Makefile
@@ -2,10 +2,22 @@ dmg := dmg
 dmg_files := $(shell find $(dmg)/ -name *.dmg)
 
 fonts := fonts
+fonts_dirs := $(foreach dmg_file,$(dmg_files),$(fonts)/$(basename $(notdir $(dmg_file))))
+
+extract_exec := extract_fonts_from_dmg.sh
 
 
 .PHONY: all
-all: dmg
+all: fonts
+
+.PHONY: fonts
+fonts: dmg $(fonts_dirs)
+ifndef fonts_dirs
+	$(MAKE) fonts
+endif
+
+$(fonts)/%: $(dmg)/%.dmg
+	bash $(extract_exec) "$<" "$@"
 
 # This target to ensure that the .dmg files are there
 # Otherwise they will be re-downloaded
--- extract_fonts_from_dmg.sh
+++ extract_fonts_from_dmg.sh
@@ -1,26 +1,25 @@
-dmg_file="$1"
-
-if [ "$dmg_file" == "" ]
+if [ $# -lt 2 ]
 then
-  echo "Usage: bash $0 <.dmg_file_to_extract>"
+  echo "Usage: bash $0 \"<.dmg_file_to_extract>\"" "<extract_directory>"
   exit 1
 fi
 
-fonts_dir="fonts"
+dmg_file="$1"
+extract_dir="$2"
 
 # extract fonts from a .dmg file
-# Usage: extract_fonts_from_dmg "<.dmg_file_to_extract>"
+# Usage: extract_fonts_from_dmg "$dmg_file" "$extract_dir"
 function extract_fonts_from_dmg {
   # 1. create target directory for fonts
-  local dmg_file="$1"
-  local basename="$(basename ""${dmg_file.%}"")"
-  local target_dir="$fonts_dir/$basename"
-  mkdir -pv "$target_dir"
+  local extract_dir="$2"
+  mkdir -pv "$extract_dir"
 
   # 2. create temporary directory to extract files
   local tempdir=$(mktemp -d)
 
   # 3. convert .dmg to .img, and put int in temporary directory
+  local dmg_file="$1"
+  local basename="$(basename ""${dmg_file.%}"")"
   local img_file="$tempdir/$basename.img"
   dmg2img -i "$dmg_file" -o "$img_file"
 
@@ -39,10 +38,10 @@ function extract_fonts_from_dmg {
   # 7. go back to current directory
   # move all .otf and .ttf files to target directory
   popd
-  find "$tempdir" -name "*.[ot]tf" -exec mv {} "$target_dir" ";"
+  find "$tempdir" -name "*.[ot]tf" -exec mv {} "$extract_dir" ";"
 
   # 8. clean up temporary directory
   rm -rf "$tempdir"
 }
 
-extract_fonts_from_dmg "$dmg_file"
+extract_fonts_from_dmg "$dmg_file" "$extract_dir"
